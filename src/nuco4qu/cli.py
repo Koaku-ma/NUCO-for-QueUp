c00095167b7ab033cd6cc96adbf794549f50c18c
NUCO <93378833+sitting-cat@users.noreply.github.com>
2024-06-27 15:08:26 +0900

    feat: config等からルーム名を読み込むようにする

--- src/nuco4qu/cli.py
+++ src/nuco4qu/cli.py
@@ -1,11 +1,14 @@
 import requests
+from decouple import config
 
 
 def main():
     ROOM_ENDPOINT = "https://api.queup.net/room/"
     CHAT_ENDPOINT = "https://api.queup.net/chat/"
+    room_name = config("ROOM_NAME", default=None)
 
-    room_name = input("Enter room name: ")
+    if room_name is None:
+        room_name = input("Enter room name: ")
 
     resp = requests.get(ROOM_ENDPOINT + room_name)
     room_id = resp.json()["data"]["_id"]
